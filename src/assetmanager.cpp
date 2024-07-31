fc338cc2144d931740e9d3eda82548d30ac5fb4f
yousmii <yousra.smits@hotmail.com>
2024-07-31 17:51:51 +0200

    [REP] Asset Manager added and implemented

---
+++ src/assetmanager.cpp
@@ -0,0 +1,32 @@
+// Source: SFML Game Engine by Sonar Systems
+#include "assetmanager.hpp"
+
+
+
+namespace Rep {
+void AssetManager::LoadTexture(const std::string& name, const std::string& fileName) {
+    sf::Texture newtexture;
+
+    if (newtexture.loadFromFile(fileName)) {
+        _textures[name] = newtexture;
+    }
+    else {
+        std::cerr << "Failed to load texture: " << fileName << std::endl;
+    }
+}
+
+sf::Texture& AssetManager::GetTexture(const std::string& name) { return _textures.at(name); }
+
+void AssetManager::LoadFont(const std::string& name, const std::string& fileName) {
+    sf::Font newfont;
+
+    if (newfont.loadFromFile(fileName)) {
+        _fonts[name] = newfont;
+    }
+    else {
+        std::cerr << "Failed to load font: " << fileName << std::endl;
+    }
+}
+
+sf::Font& AssetManager::GetFont(const std::string& name) { return _fonts.at(name); }
+} // namespace Rep
---
+++ src/assetmanager.hpp
@@ -0,0 +1,30 @@
+// Source: SFML Game Engine by Sonar Systems
+
+#ifndef ASSETMANAGER_HPP
+#define ASSETMANAGER_HPP
+
+#include <SFML/Graphics.hpp>
+#include <map>
+#include <iostream>
+
+namespace Rep {
+
+class AssetManager {
+public:
+    AssetManager() = default;
+    ~AssetManager() = default;
+
+    void LoadTexture(const std::string& name, const std::string& fileName);
+    sf::Texture &GetTexture(const std::string& name);
+
+    void LoadFont(const std::string& name, const std::string& fileName);
+    sf::Font &GetFont(const std::string& name);
+
+private:
+    std::map<std::string, sf::Texture> _textures;
+    std::map<std::string, sf::Font> _fonts;
+};
+
+} // Rep
+
+#endif //ASSETMANAGER_HPP
